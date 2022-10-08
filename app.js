305f636ed49926f0ce06be7f7ab266070e9533ff
Roger Edward Poirier III <tpoirier1127@gmail.com>
2022-10-08 18:50:57 -0500

    Added phrases to app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var qwerty = document.getElementById('qwerty');
-var phrase = document.getElementById('phrase');
-var start = document.getElementsByClassName('btn__reset');
-var missed = 0;
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+let start = document.getElementsByClassName('btn__reset');
+const missed = 0;
 
 start[0].addEventListener('click', () => {
-    var overlay = document.getElementById('overlay');
+    const overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 });
+const phrases = ['The best of both worlds','Let the cat out of the bag','To cost an arm and a leg','Getting a taste of your own medicine','To hit the nail on the head'];
