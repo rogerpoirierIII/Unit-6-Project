d2b5a331699eabd32fade02ee3278ba85e6585e8
Roger Edward Poirier III <tpoirier1127@gmail.com>
2022-10-09 11:13:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
+// Global Variables===============================================================
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-let start = document.getElementsByClassName('btn__reset');
-const missed = 0;
-
-// Listens for the start game button to be pressed to hide the overlay
-start[0].addEventListener('click', () => {
-    const overlay = document.getElementById('overlay');
-    overlay.style.display = 'none';
-});
-const phrases = ['The best of both worlds','Let the cat out of the bag','To cost an arm and a leg','Getting a taste of your own medicine','To hit the nail on the head'];
+const start = document.getElementsByClassName('btn__reset');
+btn = document.getElementsByTagName('BUTTON')
+let missed = 0;
 
 // Returns a random phrase from the phrases array
-const getRandomPhraseAsArray = arr => {   
+function getRandomPhraseAsArray(){   
     let phraseNum = Math.floor(Math.random(phrases.length)*(phrases.length+1))
     return phraseNum;
 }
+//================================================================================
+
+// Functions======================================================================
+
 // Add the leteters of a string to the display
-const addPhraseToDisplay= arr =>{
-    let currentPhrase = phrases[getRandomPhraseAsArray(phrases)].split('');   
+function addPhraseToDisplay(){
+    let currentPhrase = phrases[getRandomPhraseAsArray(phrases)];
+    currentPhrase.split('');   
     for(let i = 0;i < currentPhrase.length;i++){
     const letter = document.createElement('li');
     letter.textContent = currentPhrase[i];
@@ -27,27 +27,46 @@ const addPhraseToDisplay= arr =>{
         letter.className = 'letter'
     }
     phrase.appendChild(letter);
-    console.log(letter)
     }
-    return letter;
 }
-
 // Check if a letter is in the phrase
-const checkLetter = button =>{
-    let letter = documnet.getElementsByClassName('letter');
+function checkLetter(button){
+    let letter = document.getElementsByClassName('letter');
     let match = null;
+
     for (let i=0; i < letter.length; i++){
-        if(button.textContent === letter[i].textContent){
+        let text = letter[i].textContent;
+        if(letter[i].innerHTML === btn.textContent){
             letter[i].className='show';
             match = button.textContent;
-
+            letter[i].className = 'show';
         }
     }
     return match;
 }
+// ===============================================================================
 
-qwerty.addEventListener('click', () => {
-    
+// Listens for the start game button to be pressed to hide the overlay
+start[0].addEventListener('click', () => {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+});
+const phrases = ['the best of both worlds','let the cat out of the bag','to cost an arm and a leg','getting a taste of your own medicine','to hit the nail on the head'];
+
+
+getRandomPhraseAsArray();
+addPhraseToDisplay();
+
+qwerty.addEventListener('click', (e) => {
+    if(e.target.className !== 'chosen'&& e.target.className !== 'keyrow'){
+        e.target.className = 'chosen';
+        let guess =checkLetter();
+        if(guess === null){
+            document.querySelector('ol').removeChild;
+            missed++;
+        }
+
+    }
 })
 
 // Check if the game has been won or lost
